2d8f9e1c44d1797c612f977e36b345babd1c2c72
kyle5408 <kyle5408@yahoo.com.tw>
2021-07-05 12:16:14 +0800

    1. 新增Goolge Map導向 2. 修正Font Awesome無法顯示問題 3. 移除沒有意義的註解及測試用Console.log

---
+++ app.js
@@ -0,0 +1,37 @@
+// ------------- 基本設定-------------
+const express = require('express')
+const restaurants = require('./restaurant.json')
+const app = express()
+const port = 3000
+const exhbs = require('express-handlebars')
+app.engine('handlebars', exhbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+
+// ------------- 回應-------------
+app.get('/', (req, res) => {
+  const restaurant = restaurants.results
+  res.render('index', { restaurants: restaurant })
+})
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant_id = req.params.restaurant_id
+  const showRestaurant = restaurants.results.find(restaurant => restaurant.id === Number(restaurant_id))
+  res.render('show', { showRestaurant: showRestaurant })
+})
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  console.log(keyword)
+  const searchResult = restaurants.results.filter(restaurant =>
+    restaurant.name.toUpperCase().includes(keyword.toUpperCase())
+    || restaurant.category.toUpperCase().includes(keyword.toUpperCase())
+  )
+  res.render('index', { restaurants: searchResult, keyword: keyword })
+})
+
+
+
+//--------------啟動並監聽伺服器-------------
+app.listen(port, () => {
+  console.log(`The server is running in http://localhost:${port}`)
+})
