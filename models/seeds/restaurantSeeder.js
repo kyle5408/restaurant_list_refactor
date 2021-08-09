614c4567e06422be243e31df3e2f6d9329676715
kyle5408 <kyle5408@yahoo.com.tw>
2021-08-09 10:19:50 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,22 +1,54 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const seedList = require('../restaurant.json')
 const seed = seedList.results
 const db = require('../../config/mongoose')
 
+const SEED_USERS = [{
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678',
+  restaurant: seed.slice(0, 3)
+},
+{
+  name: 'user2',
+  email: 'user2@example.com',
+  password: '12345678',
+  restaurant: seed.slice(3, 6)
+},
+]
+
 db.once('open', () => {
-  seed.forEach(restaurant => {
-    Restaurant.create({
-      id: restaurant.id,
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
+  Promise.all(
+    Array.from(SEED_USERS, (SEED_USER, i) => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER.password, salt))
+        .then(hash =>
+          User.create({
+            name: SEED_USER.name,
+            email: SEED_USER.email,
+            password: hash
+          })
+        )
+        .then(user => {
+          const restaurantList = SEED_USER.restaurant
+          return Promise.all(Array.from(restaurantList, (restaurant, i) => {
+            const { name, name_en, category, image, location, phone, google_map, rating, description } = restaurant
+            const userId = user._id
+            return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description, userId })
+          }))
+        })
     })
-  })
-  console.log('Restaurant update successful!')
+  )
+    .then(() => {
+      console.log('Restaurant update successful!')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
